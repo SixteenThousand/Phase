390d14ea6448a6258818a1a14bd81145a90e44ad
Thomas Lindop <thomas16256@gmail.com>
2024-08-12 10:16:15 +0100

    >> rename stuff in test to align with rename of process_escapes

--- test.py
+++ test.py
@@ -3,23 +3,24 @@ import phase
 DATA_DIR = "./tests/test-data"
 TEST_DIR = "./tests"
 
+
 def main():
-    assert process_escapes_test()
+    assert pat_to_regex_test()
     print("All tests passed!")
 
-
-def process_escapes_test() -> bool:
+def pat_to_regex_test() -> bool:
     examples = {
         "boring_stuff_v%V.txt": r"boring_stuff_v(\d+)\\.txt",
         "TooMany%%VpercentSigns%V.py": r"TooMany%VpercentSigns(\d+)\\.py",
     }
     for filename in examples:
-        result = phase.process_escapes(filename)
+        result = phase.pat_to_regex(filename)
         if result != examples[filename]:
             print(f"Example {filename} failed!")
             print(f"    result: {result}")
             print(f"    expected: {examples[filename]}")
             return False
     return True
+
 
 if __name__ == "__main__": main()
