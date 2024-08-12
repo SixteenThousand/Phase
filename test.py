3d195a4244b52ada57199dcef7f852b5807fb7f1
Thomas Lindop <thomas16256@gmail.com>
2024-08-12 12:59:36 +0100

    >> start writing get_versions tests

--- test.py
+++ test.py
@@ -10,6 +10,28 @@ def main():
     seed()
     print("All tests passed!")
 
+def seed():
+    os.chdir(DATA_DIR)
+    for file in os.listdir("."):
+        os.system("rm -r "+file)
+    for i in range(1,6):
+        Path(f"boring_stuff_v{i}.ods").touch(exist_ok=False)
+        Path(f"boring_padded_v{i:03}.ods").touch(exist_ok=False)
+        Path(f"boring_offset_v{i+10}.ods").touch(exist_ok=False)
+    for i in range(1,21):
+        Path(f"interesting_stuff_v{i}.pdf").touch(exist_ok=False)
+        Path(f"interesting_padded_v{i:03}.pdf").touch(exist_ok=False)
+        Path(f"sometimes_padded_v{i:02}.pdf").touch(exist_ok=False)
+    for i in range(1,12):
+        Path(f"fascinating_stuff_v{i}").touch(exist_ok=False)
+        Path(f"fascinating_padded_v{i:03}").touch(exist_ok=False)
+    for i in range(1,21):
+        os.mkdir(f"interesting_dir_v{i}")
+    Path("dud").touch(exist_ok=False)
+    Path("oring_stuff_1.ods").touch(exist_ok=False)
+    Path("boring_stuff_v1Xods").touch(exist_ok=False)
+    Path("boring_stuffv1Xods").touch(exist_ok=False)
+
 def pat_to_regex_test() -> bool:
     examples = {
         "boring_stuff_v%V.txt": r"boring_stuff_v(\d+)\\.txt",
@@ -24,26 +46,20 @@ def pat_to_regex_test() -> bool:
             return False
     return True
 
-def seed():
-    os.chdir(DATA_DIR)
-    for file in os.listdir("."):
-        os.system("rm -r "+file)
-    for i in range(1,6):
-        Path(f"boring_stuff_v{i}.ods").touch(exist_ok=False)
-        Path(f"boring_padded_v{i:03}.ods").touch(exist_ok=False)
-    for i in range(1,21):
-        Path(f"interesting_stuff_v{i}.pdf").touch(exist_ok=False)
-        Path(f"interesting_padded_v{i:03}.pdf").touch(exist_ok=False)
-        Path(f"sometimes_padded_v{i:02}.pdf").touch(exist_ok=False)
-    for i in range(1,12):
-        Path(f"fascinating_stuff_v{i}").touch(exist_ok=False)
-        Path(f"fascinating_padded_v{i:03}").touch(exist_ok=False)
-    for i in range(1,21):
-        os.mkdir(f"interesting_dir_v{i}")
-    Path("dud").touch(exist_ok=False)
-    Path("oring_stuff_1.ods").touch(exist_ok=False)
-    Path("boring_stuff_v1Xods").touch(exist_ok=False)
-    Path("boring_stuffv1Xods").touch(exist_ok=False)
+def get_versions_test() -> bool:
+    seed()
+    examples = {
+        r"boring_stuff_v(\d+)\.ods":
+            [(f"boring_stuff_v{i}.ods",i) for i in range(1,6)],
+        r"boring_padded_v(\d)\.ods":
+            [(f"boring_padded_v{i:03}.pdf",i) for i in range(1,6)],
+        r"boring_offset_v(\d)\.ods":
+            [(f"boring_padded_v{i+10}.pdf",i) for i in range(1,6)],
+        r"sometimes_padded_v(\d+)\.pdf":
+            [(f"sometimes_padded_v{i:02},i) for i in range(1,21)],
+        r"interesting_stuff_v(\d+)\.
+    }
+    
 
 
 if __name__ == "__main__": main()
